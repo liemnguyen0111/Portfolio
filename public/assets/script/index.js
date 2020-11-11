f4c2f707705d522b9ce9490e8f656af32ab53344
Liem Nguyen <liemnguyen0111@gmail.com>
2020-11-10 21:03:10 -0800

    finishing up about page

--- public/assets/script/index.js
+++ public/assets/script/index.js
@@ -1,30 +1,26 @@
-const container = document.querySelector('.achievement')
-const card = document.querySelector('.card')
-
-const title = document.querySelector('.card p')
-const img = document.querySelector('.card img')
-
-$('.card').mousemove( e =>
+const menu = document.querySelector('.menu')
+let open = false
+document.addEventListener('click', e =>
 {
- 
-        let xAxis = (window.innerWidth / 2 - e.pageX) / 25
-        let yAxis = (window.innerHeight / 2 - e.pageY) / 25
-        console.log(card)
-      e.target.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+        e.preventDefault()
+        if(e.target.name === 'menu')
+        {
+                console.log('clicked')
+                if(open)
+                {
+                        open = false
+                        menu.style.display = 'none'
+                }
+                else
+                {
+                        open = true
+                        menu.style.display = 'block'
+                }
+                
+        }
+        else
+        {
+                open = false
+                menu.style.display = 'none'
+        }
 })
-
-$('.card').mouseenter( e =>
-        {
-               e.target.style.transition = "none"
-               title.style.transform = 'translateZ(50px)'
-               img.style.transform = 'translateZ(50px)'
-        })
-
-$('.card').mouseleave( e =>
-        {
-                e.target.style.transition = "all 0.5s ease"
-                e.target.style.transform = `rotateY(0deg) rotateX(0deg)`
-
-                title.style.transform = 'translateZ(0px)'
-                img.style.transform = 'translateZ(0px)'
-        })
